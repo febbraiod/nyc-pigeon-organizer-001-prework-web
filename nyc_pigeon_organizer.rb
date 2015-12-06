6b2ea095a552c637a61f44ef950e56181c295a9f
febbraiod <donedesign1@gmail.com>
2015-12-06 13:00:16 -0500

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,24 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeons = {}
+  keys = data[:color].values.flatten.uniq
+
+  keys.each do |name|
+    pigeons[name] = {}
+  end
+
+  keys.each do |name|
+    data.each do |qualitys, data_hash|
+      quality_arr = []
+      data_hash.each do |quality, names|
+        if names.include?(name)
+          quality_arr << quality.to_s
+          pigeons[name][qualitys] = quality_arr
+        end
+      end
+    end
+  end
+
+  pigeons
 end
